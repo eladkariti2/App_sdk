f44936aa0c2e3c25b2ada8b90c8b4b28b3f0939c
elad kariti <e.kariti@applicaster.com>
2014-07-18 13:43:51 +0300

    add description to image holdeR

--- src/com/application/imageholders/ImageHolder.java
+++ src/com/application/imageholders/ImageHolder.java
@@ -4,9 +4,49 @@ import java.util.HashMap;
 
 public class ImageHolder {
 
-	String mImageUrl;
+	private String mImageUrl;
 	String mTitle;
+	String mDescription;
 	
-	HashMap<String, String> mExtension; 
+	HashMap<String, String> mExtension;
+
+	public ImageHolder(String title,String url){
+		this(title,"",url);
+	}
+	
+	public ImageHolder(String title,String description,String url){
+		mImageUrl = url;
+		mTitle = title;
+		mExtension = new HashMap<String, String>();
+	}
+	
+	
+	
+	
+	public String getImageUrl() {
+		return mImageUrl;
+	}
+
+	public String getImageTitle() {
+		return mTitle;
+	}
+	
+	public String getDescriptionUrl() {
+		return mDescription;
+	}
+	
+	public String getExtension(String key){
+		String value = "";
+		
+		if(mExtension.containsKey(key)){
+			value = mExtension.get(key);
+		}
+		
+		return value;
+	}
+	
+	public void addExtension(String key,String value){
+		mExtension.put(key, value);
+	}
 	
 }
