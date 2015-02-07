b53dbc63f861478a575b56c88395595156f146d7
elad kariti <e.kariti@applicaster.com>
2015-02-07 13:51:55 +0200

    delete old facebook use

--- src/com/application/facebook/interfaces/FacebookLoaderI.java
+++
@@ -1,9 +0,0 @@
-package com.application.facebook.interfaces;
-
-import com.application.facebook.model.FbModel;
-
-public interface FacebookLoaderI {
-
-	public void onSuccess(FbModel model);
-	public void onFailure(Exception e);	
-}
