0ceaa1596f1819ccaa5a87e9f0c7c7cd290b7df5
elad kariti <e.kariti@applicaster.com>
2015-09-19 15:21:07 +0300

    use volley remove picaso

---
+++ src/com/application/interfaces/OnCustomImageLoaderListener.java
@@ -0,0 +1,7 @@
+package com.application.interfaces;
+
+/**
+ * Created by elad on 9/19/2015.
+ */
+public class OnCustomImageLoaderListener {
+}
---
+++ src/com/application/utils/CustomImageLoader.java
@@ -0,0 +1,7 @@
+package com.application.utils;
+
+/**
+ * Created by elad on 9/19/2015.
+ */
+public class CustomImageLoader {
+}
